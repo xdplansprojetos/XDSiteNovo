c3850c80a372978cbf7a873676e983d24b153ded
dx <dx@kali.dx>
2022-09-26 20:56:00 -0300

    Atualizacao TRES

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -63,9 +63,9 @@ const FeaturesTiles = ({
                 <div className="features-tiles-item-header">
                   <div className="features-tiles-item-image mb-16">
                     <Image
-                      src={require('./../../assets/images/feature-tile-icon-01.svg')}
+                      src={require('./../../assets/images/marketing-digital.png')}
                       alt="Features tile icon 01"
-                      width={64}
+                      width={250}
                       height={64} />
                   </div>
                 </div>
