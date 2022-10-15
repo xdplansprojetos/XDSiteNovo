739bbe0842eb45abb1aee599ceeb2074f93804eb
David Xavier <webxddesign@gmail.com>
2022-10-15 18:00:00 -0300

    Nova att

--- src/views/Contato.js
+++ src/views/Contato.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import FeaturesSplit from '../components/sections/FeaturesSplit'
 
 export default function Contato () {
 
     return (
         <>
             <h1>Teste</h1>
+            <FeaturesSplit />
         </>
     )
 }
