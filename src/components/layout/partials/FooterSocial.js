2875e567b7da834b11b742c3ad8ed3e0f34c3b3e
David Xavier <webxddesign@gmail.com>
2022-10-21 07:17:27 -0300

    Instagram

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -44,7 +44,7 @@ const FooterSocial = ({
           </a>
         </li>
         <li>
-          <a href="https://google.com/">
+          <a href="https://instagram.com/xd_plans">
             <svg
               width="16"
               height="16"
