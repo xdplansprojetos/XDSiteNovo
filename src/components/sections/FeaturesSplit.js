4c8a408e73e17e165a295c438dc410334417ac1c
David Xavier <webxddesign@gmail.com>
2022-11-03 23:25:50 -0300

    App Att Text

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -119,13 +119,13 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  Crie Seu Aplicativo
                 </div>
                 <h3 className="mt-0 mb-12">
                   Criação de Aplicativo
                 </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Criamos o Aplicativo que pode ser Atualizado com seu Site Atual, e acrescentar novas características proprias para Captar Informações e Engajamento dentro da sua empresa, por meio nativo do Android e iOS.
                 </p>
               </div>
               <div className={
